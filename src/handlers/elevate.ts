5fac4cf6db5dbf000b424d091a1caed92877d09e
Freddie <tsfreddiewang@gmail.com>
2022-11-02 23:54:10 +0800

    elevate fix

--- src/handlers/elevate.ts
+++ src/handlers/elevate.ts
@@ -12,7 +12,7 @@ export const elevated =
       .split(':')[0];
 
     // check if accessing this user's database
-    if (!url.match(new RegExp(`^\/clarkchen(?:-[^\s\/\\]*)?(?:\/|$)`))) {
+    if (!url.match(new RegExp(`^\\/${username}(?:-[^\\s\\/\\\\]*)?(?:\\/|$)`))) {
       return reply.code(403).send({ message: 'invalid access' });
     }
 
